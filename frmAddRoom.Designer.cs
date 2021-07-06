9f55fc1755380f1bd2072330be87b8643b569140
tieuyennhu16 <85612790+tieuyennhu16@users.noreply.github.com>
2021-07-06 14:48:03 +0700

    Create frmAddRoom.Designer.cs

---
+++ frmAddRoom.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace MotelRoomManagement
+{
+    partial class frmAddRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbLoaiPhong = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.cbKV = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btHuy = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTenPhong = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.cbLoaiPhong);
+            this.panel1.Controls.Add(this.cbKV);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btHuy);
+            this.panel1.Controls.Add(this.btThem);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtTenPhong);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtDay);
+            this.panel1.Controls.Add(this.txtMaPhong);
+            this.panel1.Location = new System.Drawing.Point(13, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 239);
+            this.panel1.TabIndex = 6;
+            // 
+            // cbLoaiPhong
+            // 
+            this.cbLoaiPhong.DisplayMember = "Text";
+            this.cbLoaiPhong.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbLoaiPhong.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbLoaiPhong.FormattingEnabled = true;
+            this.cbLoaiPhong.ItemHeight = 27;
+            this.cbLoaiPhong.Location = new System.Drawing.Point(461, 12);
+            this.cbLoaiPhong.Name = "cbLoaiPhong";
+            this.cbLoaiPhong.Size = new System.Drawing.Size(94, 33);
+            this.cbLoaiPhong.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cbLoaiPhong.TabIndex = 5;
+            // 
+            // cbKV
+            // 
+            this.cbKV.DisplayMember = "Text";
+            this.cbKV.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbKV.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbKV.FormattingEnabled = true;
+            this.cbKV.ItemHeight = 27;
+            this.cbKV.Location = new System.Drawing.Point(161, 71);
+            this.cbKV.Name = "cbKV";
+            this.cbKV.Size = new System.Drawing.Size(131, 33);
+            this.cbKV.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cbKV.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(305, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tên Phòng:";
+            // 
+            // btHuy
+            // 
+            this.btHuy.BackColor = System.Drawing.Color.Red;
+            this.btHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btHuy.Location = new System.Drawing.Point(322, 185);
+            this.btHuy.Name = "btHuy";
+            this.btHuy.Size = new System.Drawing.Size(196, 42);
+            this.btHuy.TabIndex = 3;
+            this.btHuy.Text = "Hủy";
+            this.btHuy.UseVisualStyleBackColor = false;
+            this.btHuy.Click += new System.EventHandler(this.btHuy_Click);
+            // 
+            // btThem
+            // 
+            this.btThem.BackColor = System.Drawing.Color.Lime;
+            this.btThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.Location = new System.Drawing.Point(67, 185);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(196, 42);
+            this.btThem.TabIndex = 3;
+            this.btThem.Text = "Thêm";
+            this.btThem.UseVisualStyleBackColor = false;
+            this.btThem.Click += new System.EventHandler(this.btThem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã Phòng:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(305, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Loại phòng:";
+            // 
+            // txtTenPhong
+            // 
+            this.txtTenPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenPhong.Location = new System.Drawing.Point(461, 71);
+            this.txtTenPhong.Name = "txtTenPhong";
+            this.txtTenPhong.Size = new System.Drawing.Size(94, 35);
+            this.txtTenPhong.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 29);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Dãy:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Khu Vực:";
+            // 
+            // txtDay
+            // 
+            this.txtDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDay.Location = new System.Drawing.Point(161, 126);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(131, 35);
+            this.txtDay.TabIndex = 4;
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaPhong.Location = new System.Drawing.Point(161, 13);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(131, 35);
+            this.txtMaPhong.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(161, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 76);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Thêm Phòng";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Yellow;
+            this.label11.Location = new System.Drawing.Point(99, 345);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 76);
+            this.label11.TabIndex = 8;
+            // 
+            // frmAddRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(599, 328);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAddRoom";
+            this.Text = "Thêm phòng";
+            this.Load += new System.EventHandler(this.frmAddRoom_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btHuy;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTenPhong;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.TextBox txtMaPhong;
+        private System.Windows.Forms.Label label1;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbLoaiPhong;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbKV;
+        private System.Windows.Forms.Label label11;
+
+    }
+}
