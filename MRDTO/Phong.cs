31c2e0d8ccaf4a8a810a9fb64341fbe91ffe1acc
tieuyennhu16 <85612790+tieuyennhu16@users.noreply.github.com>
2021-07-06 14:48:33 +0700

    Create Phong.cs

---
+++ MRDTO/Phong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MRDTO
+{
+    public class Phong
+    {
+        public string maphong { get; set; }
+        public string maloai { get; set; }
+        public string KhuVuc { get; set; }
+        public string tenphong { get; set; }
+        public string Day { get; set; }
+        public string Trangthai { get; set; }
+        public Phong(string maphong, string maloai, string KhuVuc, string tenphong, string Day, string Trangthai)
+        {
+            this.maphong = maphong;
+            this.maloai = maloai;
+            this.KhuVuc = KhuVuc;
+            this.tenphong = tenphong;
+            this.Day = Day;
+            this.Trangthai = Trangthai;
+        }
+    }
+}
